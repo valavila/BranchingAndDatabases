d2b86cb7043ae88df071ee5e3d932967dd6de980
Valeria Avila <valeria.avila17@gmail.com>
2020-07-19 14:49:44 -0700

    Created a class for comp equipment

---
+++ BranchingAndDatabasePractice/Equipment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAndDatabasePractice
+{
+    /// <summary>
+    /// Equiment represents a single piece of computer equipment
+    /// </summary>
+    class Equipment
+    {
+        /// <summary>
+        /// Primary key of the equipment
+        /// </summary>
+        public int EquipmentId { get; set; }
+
+        /// <summary>
+        /// Name of equipment
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Retail price of the equipment
+        /// </summary>
+        public double Price { get; set; }
+    }
+
+}
